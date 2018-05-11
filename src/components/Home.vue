2ead413bd7be7cfd62934661ba6e9d2875b4e976
chanlumin <chanlumin@163.com>
2018-05-11 12:15:46 +0800

    init project

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="Home">
+    Home
+    <div id="test">
+      测试测试测试测试测试测试测试测试测试测试
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+  @import "~@/assets/css/common/ellipse.styl"
+  #test
+    width:20px
+    ellipse()
+
+</style>
